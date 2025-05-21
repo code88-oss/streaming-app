35fb354f3f8edc2f4a40f3794d33a46f3b07a819
Thai Tran Hoang <tranhoangthaiair1998@gmail.com>
2025-05-21 17:42:59 +0700

    update oauth service

--- app/presentation/components/social-login-buttons.tsx
+++ app/presentation/components/social-login-buttons.tsx
@@ -1,9 +1,12 @@
 export default function SocialLoginButtons() {
   return (
-    <div className="mt-4 flex justify-center gap-4">
+    <a
+      href={`${process.env.NESTJS_API_URL}/auth/google`}
+      className="mt-4 flex justify-center gap-4"
+    >
       <button className="bg-[#ff0000] text-white px-4 py-2 rounded-md hover:bg-[#cc0000] w-full">
         Google
       </button>
-    </div>
+    </a>
   );
 }
